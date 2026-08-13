48321fc22db4c873b1f59f172fc2f3f1003a6dfe
Izzahafkar1na <izzahafkarinakarin810@gmail.com>
2026-08-13 09:19:00 +0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,11 @@
+# Program Input Output String —izzah afkarina
+nama = "karin"
+kelas = "10A"
+hobi = "masak"
+
+print("\n===== HASIL DATA =====")
+print(f"izzah afkarina: {karin}")
+print(f"saya kelas x: {x}")
+print(f"Hobi saya masak: {masak}")
+
+print(f"\nDibuat oleh: karin")
